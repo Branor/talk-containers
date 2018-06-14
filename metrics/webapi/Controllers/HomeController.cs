bca721931f2b415d3caa982af7cc5f51a10f72d6
David Ostrovsky <dostrovsky@proofpoint.com>
2018-06-14 10:48:48 +0200

    Updated the presentation and switched from F# to C# for the dotnet demo.

--- metrics/webapi/Controllers/HomeController.cs
+++ metrics/webapi/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.AspNetCore.Mvc;
 using StatsN;
 
@@ -15,34 +16,24 @@ namespace webapi.Controllers
             this.stats = stats;
         }
 
-        [HttpGet("application")]
+        [HttpGet("/")]
         public async Task<string> Get()
         {
-            await stats.CountAsync("get");
+            await stats.CountAsync("get"); // Aggregated metrics
+            await stats.CountAsync($"{System.Environment.MachineName}.get"); // Contaner metric
+            
             return $"[FROM: {System.Environment.MachineName}]: Hello world!";
         }
 
-        [HttpGet("application/{id}")]
+        [HttpGet("/{id}")]
         public async Task<string> Get(int id)
         {
-            // Probably a bad idea, metric with high cardinality!
-            await stats.CountAsync($"get.{id}"); 
-            return $"[FROM: {System.Environment.MachineName}]: Let's play Good Idea - Bad Idea!";
-        }
-
-        [HttpGet("container")]
-        public async Task<string> GetMachine()
-        {
-            await stats.CountAsync($"{System.Environment.MachineName}.get");
-            return $"[FROM: {System.Environment.MachineName}]: You get a container, and you get a container, everybody gets a container!";
-        }
-
-        [HttpGet("container/{id}")]
-        public async Task<string> GetMachine(int id)
-        {
-            // Terrible idea, metric with very high cardinality!
+            // Might be a bad idea, metric with high cardinality.
+            await stats.CountAsync($"get.{id}");
+            // Almost certainly a bad idea, very high cardinality!
             await stats.CountAsync($"{System.Environment.MachineName}.get.{id}");
-            return $"[FROM: {System.Environment.MachineName}]: So many metrics! You'll get tired of metrics!";
+
+            return $"[FROM: {System.Environment.MachineName}]: Let's play Good Idea - Bad Idea!";
         }
     }
 }
